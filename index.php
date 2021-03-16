ff8d3ea5fa5b25cc4113aecb3a857cf53b708703
luebracho <40438495+luebracho@users.noreply.github.com>
2021-03-16 14:11:15 -0500

    empezando php

--- index.php
+++ index.php
@@ -1,24 +1,11 @@
-<!--
-    Winhweel.js basic code wheel example by Douglas McKechie @ www.dougtesting.net
-    See website for tutorials and other documentation.
-    The MIT License (MIT)
-    Copyright (c) 2016 Douglas McKechie
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
--->
+<?php
+//iniciamos con el backend
+
+
+
+
+?>
+
 
 <html>
     <head>
@@ -471,9 +458,12 @@
                     // note the indicated segment is passed in as a parmeter as 99% of the time you will want to know this to inform the user of their prize.
                     // -------------------------------------------------------
                     function alertPrize(indicatedSegment)
+                    <?php 
+                    $ruleta = "indicatedSegment.text";
+                    ?>
                     {
                     // Do basic alert of the segment text. You would probably want to do something more interesting with this information.
-                    alert("Total " + indicatedSegment.text);
+                    alert("Total " + <?php echo $ruleta;?>);
                     }
 
                     $('body').keyup(function(e) {
